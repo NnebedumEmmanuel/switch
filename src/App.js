cf161d822ce18253f112a65a9f5df5b7145954fc
NnebedumEmmanuel <emmanuelnnebedum704@gmail.com>
2023-11-06 10:33:06 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link, Routes } from "react-router-dom";
+import ProductList from "./";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      return (
+      <Router>
+       <nav>
+                <ul>
+                    <li>
+                        <Link to="/products/category1">Category 1</Link>
+                    </li>
+                    <li></li>
+                        <Link to="/products/category2">Category 2</Link>
+                    </li>
+                    <li>
+                        <Link to="/products/category3">Category 3</Link>
+                    </li>
+                </ul>
+            </nav>
+
+        <Routes>
+          <Route
+            path="/products/category1"
+            component={() => <ProductList category="category1" />}
+          />
+          <Route
+            path="/products/category2"
+            component={() => <ProductList category="category2" />}
+          />
+          <Route
+            path="/products/category3"
+            component={() => <ProductList category="category3" />}
+          />
+        </Routes>
+      </Router>
+      ); 
     </div>
   );
 }
